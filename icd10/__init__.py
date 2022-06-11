b765feff46aaf9eb3ada313375c8d12094526c15
Xiaowei Song <Xiaowei.Song@gdit.com>
2022-06-11 11:47:09 -0400

    fix bug on R000's chapter query

--- icd10/__init__.py
+++ icd10/__init__.py
@@ -123,7 +123,9 @@ def find(s: str) -> Optional[ICD10]:
 
 
 def in_chapter(block: str, icd10: str) -> bool:
-    alpha, numeric = ord(icd10[0]), int(icd10[1:3].lstrip('0'))
+    snum=icd10[1:3].lstrip('0') #deal with R000, i.e., all 0 case, Xiaowei, 20220611
+    if len(snum)=0: snum='0'
+    alpha, numeric = ord(icd10[0]), int(snum)
     sblock, eblock = block.split('-')  # A00-B99
     salpha, snumeric = ord(sblock[0]), int(sblock[1:].lstrip('0') or 0)
     ealpha, enumeric = ord(eblock[0]), int(eblock[1:].lstrip('0') or 0)
